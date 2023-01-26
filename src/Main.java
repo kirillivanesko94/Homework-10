480f4d73930bdacb524578a56ecc8c447c55604b
Kirill <kirill.ivanesko@ya.ru>
2023-01-26 11:53:35 +0300

    Homework 10 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    private static void task3() {
+        System.out.println("Задача №3");
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+        String expectedName = fullName.replace('ё', 'е' );
+        System.out.println("Данные ФИО сотрудника — " + expectedName);
+    }
+
+    private static void task2() {
+        System.out.println("Задача №2");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+        String expectedName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + expectedName);
+    }
+
+
+    private static void task1() {
+        System.out.println("Задача №1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
     }
 }
